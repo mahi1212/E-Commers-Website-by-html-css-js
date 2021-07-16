82bd412a0efa8da1bafc43e39eb68666622596c5
mahi1212 <mahi121.mr@gmail.com>
2021-07-16 20:24:02 +0600

    Fixed

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const openNav = document.querySelector(".hamburger");
 const closeNav = document.querySelector(".close");
 const menu = document.querySelector(".menu");
 
-const leftPosition = menu.getBoundingClientRect().left;
-
+// const leftPosition = menu.getBoundingClientRect().left;
+// Left position didnot worked
 openNav.addEventListener("click", () => {
-    if(leftPosition < 0){
-        menu.classList.add("show");
-    }
+    // if(leftPosition < 0){
+    //     menu.classList.add("show");
+    // }
+    menu.classList.add("show");
+    
 });
 
 closeNav.addEventListener("click", ()=> {
-    if(leftPosition < 0){
-        menu.className.remove("show");
-    }
+    // if(leftPosition < 0){
+    //     menu.className.remove("show");
+    // }
+    menu.classList.remove("show");
 });
